301a3e5fdf5719cad7520066ce57279ccdbf2361
cyberjj999 <lyejiajun99@gmail.com>
2025-03-19 16:23:36 +0800

    chore: add support for new images

--- eventgo-frontend/next.config.js
+++ eventgo-frontend/next.config.js
@@ -18,6 +18,30 @@ const nextConfig = {
 				protocol: "https",
 				hostname: "media1.s-nbcnews.com",
 			},
+			{
+				protocol: "https",
+				hostname: "tmw.com.sg",
+			},
+			{
+				protocol: "https",
+				hostname: "talkstar-photos.s3.amazonaws.com",
+			},
+			{
+				protocol: "https",
+				hostname: "www.pelago.com",
+			},
+			{
+				protocol: "https",
+				hostname: "encrypted-tbn0.gstatic.com",
+			},
+			{
+				protocol: "https",
+				hostname: "images.ra.co",
+			},
+			{
+				protocol: "https",
+				hostname: "singaporegp.sg",
+			},
 		],
 	},
 };
